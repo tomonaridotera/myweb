4139223f240f82d1c0f3735f162ca4c1abc469f1
Tomonari Dotera <dotera@phys.kindai.ac.jp>
2020-08-06 15:29:30 +0900

    no message

---
+++ scripts/main.babel.js
@@ -0,0 +1,19 @@
+$('#js-open-menu').on('click', function(){
+  $('#js-menu-wrap').show();
+});
+
+$('#js-close-menu').on('click', function(){
+  $('#js-menu-wrap').hide();
+});
+
+$('#js-toggleBtn').on('click', function(){
+  if ($(this).hasClass('en')) {
+    $(this).removeClass('en').text('English');
+    $('#js-en').slideUp();
+    $('#js-jp').slideDown();
+  } else {
+    $(this).addClass('en').text('Japanese');
+    $('#js-jp').slideUp();
+    $('#js-en').slideDown();
+  }
+});
